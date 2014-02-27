731e2ac97248775da53b43b56e3ce6e85bd9e3a5
Justin Love <git@JustinLove.name>
2014-02-27 10:35:08 -0600

    proof of concept

--- ui/mods/linger/linger.js
+++ ui/mods/linger/linger.js
@@ -1,13 +1,32 @@
 define([
-  'text!linger/linger.html'
+  'text!linger/exit_button.html'
 ], function(html) {
   "use strict";
 
+  var liveGameServerState = handlers.server_state
+  var gameOverUrl = '../game_over/game_over.html'
+
+  handlers.server_state = function(msg) {
+    if (msg.state == 'game_over') {
+      gameOverUrl = msg.url
+      msg.url = null
+    }
+    liveGameServerState(msg)
+  }
+
+  var viewModel = {
+    navToGameOver: function() {
+      model.gameOverReviewMode(false);
+      window.location.href = gameOverUrl
+    }
+  }
+
   return {
     ready: function() {
-      var container = $(html)
-      container.appendTo('body')
-      ko.applyBindings(model, container)
+      createFloatingFrame('linger_exit_button_frame', 200, 40, {'offset': 'leftCenter', 'left': 0});
+      var $container = $('#linger_exit_button_frame_content')
+      $(html).appendTo($container)
+      ko.applyBindings(viewModel, $container[0])
     }
   }
 })
